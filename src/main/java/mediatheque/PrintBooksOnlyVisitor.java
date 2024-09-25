b8492c9e635835e98999e56c6fd0100b640bd9e4
simon bodin <simon.bodin@etud.univ-jfc.fr>
2024-09-25 09:18:30 +0200

    Interface

--- src/main/java/mediatheque/PrintBooksOnlyVisitor.java
+++ src/main/java/mediatheque/PrintBooksOnlyVisitor.java
@@ -3,11 +3,9 @@ package mediatheque;
 public class PrintBooksOnlyVisitor implements ItemVisitor {
     @Override
     public void visit(Book book) {
-        System.out.println("Book: " + book.getTitle() + " de " + book.getAuthor());
     }
 
     @Override
     public void visit(CD cd) {
-        // do nothing
     }
 }
--- src/main/java/mediatheque/PrintCatalogVisitor.java
+++ src/main/java/mediatheque/PrintCatalogVisitor.java
@@ -3,11 +3,9 @@ package mediatheque;
 public class PrintCatalogVisitor implements ItemVisitor {
     @Override
     public void visit(Book book) {
-        System.out.println("Book: " + book.getTitle() + " de " + book.getAuthor());
     }
 
     @Override
     public void visit(CD cd) {
-        System.out.println("CD: " + cd.getTitle() + " (" + cd.getNumberOfTracks() + ")");
     }
 }
